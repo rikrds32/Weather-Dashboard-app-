a1656325123678a0b0cd036d32330b277453f08c
Ricardo Salazar <no-replay>
2021-06-08 00:58:21 -0500

    add click function

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ var apiKey="398dd4de7a0a8a9dafc9ebc7a298ca0b"
 var city=""
 // get weather function//
 function getWeather(city){
-    var weatherUrl="api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
-    fetch(weatherUrl)
-    .then(function(res){
-
+    var weatherUrl="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+    $.ajax({
+        url:weatherUrl,
+        method:"GET"
+    }).then(function(res){
+        $("#city-name").text(res.name)
     })
 }
+$("#search").on("click",function(){
+    city=$("#search-input").val();
+    getWeather(city);
+})
