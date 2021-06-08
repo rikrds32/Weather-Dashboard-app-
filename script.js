4702d834d67a196f49c01c8e4043a3e612fbddee
Ricardo Salazar <no-replay>
2021-06-08 00:15:54 -0500

    add getweather function

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // weather api key//
 var apiKey="398dd4de7a0a8a9dafc9ebc7a298ca0b"
+// city var//
+var city=""
+// get weather function//
+function getWeather(city){
+    var weatherUrl="api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+    $.ajax({
+        url:weatherUrl,
+        method:"GET"
+    }).then(function(res){
+
+    })
+}
